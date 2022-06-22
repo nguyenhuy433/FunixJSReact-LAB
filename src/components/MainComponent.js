d8e2afa306b7e655882c36d8b3d810dfdde7396a
Nguyen Huy <nguyenhuy64220@gmail.com>
2022-06-22 15:22:48 +0700

    react router

---
+++ src/components/MainComponent.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./MenuComponent";
+import DishDetail from "./DishDetailComponent";
+import { DISHES } from "../shared/dishes";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+import Home from "./HomeComponent";
+import { Switch, Route, Redirect } from "react-router-dom";
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
+
+  render() {
+    const HomePage = () => {
+      return <Home />;
+    };
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route
+            exact="/menu"
+            component={() => <Menu dishes={this.state.dishes} />}
+          />
+          <Redirect to="/home" />
+        </Switch>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Main;
