43383322b2b280a4eaeb7765640ca5cf4b6ef86c
Nguyen Huy <nguyenhuy64220@gmail.com>
2022-07-27 20:29:27 +0700

    React Router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Navbar, NavbarBrand } from "reactstrap";
-import Menu from "./components/MenuComponent";
-import { DISHES } from "./shared/dishes";
 import Main from "./components/MainComponent";
+import { BrowserRouter } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
