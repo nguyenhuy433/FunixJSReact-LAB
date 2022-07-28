492a743d11fdf639b1a581dc3f5c2b81d0e53994
Nguyen Huy <nguyenhuy64220@gmail.com>
2022-07-28 17:42:16 +0700

    Build

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -44,10 +44,6 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   componentDidMount() {
     this.props.fetchDishes();
     this.props.fetchComments();
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,9 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 import {
   Card,
   CardImg,
-  CardText,
-  CardBody,
   CardTitle,
   CardImgOverlay,
   Breadcrumb,
